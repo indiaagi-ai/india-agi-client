5117c22c02c14d18c08c6143d3a0186ef9af7c6a
Kishan Kokal <kokalkishan.official@gmail.com>
2025-04-15 01:14:25 +0530

    :sparkles: feat: Enhance SearchPage with dynamic provider updates and add visual indicators for active providers

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,15 +3,21 @@ import { Input } from "@/components/ui/input";
 import { GlobeIcon } from "@radix-ui/react-icons";
 import { AnimatedList } from "@/components/magicui/animated-list";
 import { useState } from "react";
-import { DebateHistory } from "@/interfaces";
+import { DebateHistory, HistoryType, Provider } from "@/interfaces";
 import { ExpandableCard } from "@/components/expandable-card";
 import { Spinner } from "@/components/ui/spinner";
 import React from "react";
+import { SparklesText } from "@/components/magicui/sparkles-text";
+import OpenAISvg from "@/assets/openai.svg";
+import GoogleSvg from "@/assets/google.svg";
+import AnthropicSvg from "@/assets/anthropic.svg";
+import XAISvg from "@/assets/xai.svg";
 
 export function SearchPage() {
   const [items, setItems] = useState<DebateHistory[]>([]);
   const [searchText, setSearchText] = useState<string>("");
   const [researching, setResearching] = useState<boolean>(false);
+  const [provider, setProvider] = useState<Provider>(Provider.OpenAI);
 
   const research = async () => {
     setResearching(true);
@@ -27,6 +33,28 @@ export function SearchPage() {
       // Handle messages
       eventSource.onmessage = ({ data }) => {
         const historyItem: DebateHistory = JSON.parse(data);
+        switch (historyItem.model) {
+          case "OpenAI":
+            if (historyItem.type === HistoryType.textResponse) {
+              setProvider(Provider.Google);
+            } else {
+              setProvider(Provider.OpenAI);
+            }
+            break;
+          case "Google":
+            if (historyItem.type === HistoryType.textResponse) {
+              setProvider(Provider.OpenAI);
+            } else {
+              setProvider(Provider.Google);
+            }
+            break;
+          case "Anthropic":
+            setProvider(Provider.Anthropic);
+            break;
+          case "xAI":
+            setProvider(Provider.xAI);
+            break;
+        }
         setItems((prevVal) => [...prevVal, historyItem]);
       };
 
@@ -49,40 +77,141 @@ export function SearchPage() {
   };
 
   return (
-    <div className="flex flex-col gap-5 w-full max-w-6xl">
-      <div className="flex flex-col items-center space-x-2 space-y-2 md:flex-row md:space-y-0">
-        <Input
-          type="search"
-          value={searchText}
-          disabled={researching}
-          onChange={(e) => setSearchText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              research();
-            }
-          }}
-          placeholder="Enter your query..."
-        />
-        <Button type="button" onClick={research} disabled={researching}>
-          {researching ? (
-            <Spinner size="sm" className="bg-black" />
-          ) : (
-            <>
-              <GlobeIcon className="mr-2 h-4 w-4" />
-              Research
-            </>
-          )}
-        </Button>
+      <div className="flex flex-col gap-5 w-full max-w-6xl">
+        <div className="flex flex-col items-center space-x-2 space-y-2 md:flex-row md:space-y-0">
+          <Input
+            type="search"
+            value={searchText}
+            disabled={researching}
+            onChange={(e) => setSearchText(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                research();
+              }
+            }}
+            placeholder="Enter your query..."
+          />
+          <Button type="button" onClick={research} disabled={researching}>
+            {researching ? (
+              <Spinner size="sm" className="bg-black" />
+            ) : (
+              <>
+                <GlobeIcon className="mr-2 h-4 w-4" />
+                Research
+              </>
+            )}
+          </Button>
+        </div>
+        <div className="w-full flex justify-center">
+          {researching &&
+            (() => {
+              switch (provider) {
+                case Provider.OpenAI:
+                  return (
+                    <div className="w-full max-w-2xl flex justify-around">
+                      <SparklesText>
+                        <img
+                          src={OpenAISvg}
+                          className="w-16 h-16 rounded-full bg-white p-1"
+                        ></img>
+                      </SparklesText>
+                      <img
+                        src={GoogleSvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      />
+                      <img
+                        src={AnthropicSvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      />
+                      <img
+                        src={XAISvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      />
+                    </div>
+                  );
+                case Provider.Google:
+                  return (
+                    <div className="w-full h-16 max-w-2xl flex justify-around">
+                      <img
+                        src={OpenAISvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      ></img>
+                      <SparklesText>
+                        <img
+                          src={GoogleSvg}
+                          className="w-16 h-16 rounded-full bg-white p-1"
+                        />
+                      </SparklesText>
+                      <img
+                        src={AnthropicSvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      />
+                      <img
+                        src={XAISvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      />
+                    </div>
+                  );
+                case Provider.Anthropic:
+                  return (
+                    <div className="w-full h-16 max-w-2xl flex justify-around">
+                      <img
+                        src={OpenAISvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      ></img>
+                      <img
+                        src={GoogleSvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      />
+                      <SparklesText>
+                        <img
+                          src={AnthropicSvg}
+                          className="w-16 h-16 rounded-full bg-white p-1"
+                        />
+                      </SparklesText>
+                      <img
+                        src={XAISvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      />
+                    </div>
+                  );
+                case Provider.xAI:
+                  return (
+                    <div className="w-full h-16 max-w-2xl flex justify-around">
+                      <img
+                        src={OpenAISvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      ></img>
+                      <img
+                        src={GoogleSvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      />
+                      <img
+                        src={AnthropicSvg}
+                        className="w-16 h-16 rounded-full bg-white p-1"
+                      />
+                      <SparklesText>
+                        <img
+                          src={XAISvg}
+                          className="w-16 h-16 rounded-full bg-white p-1"
+                        />
+                      </SparklesText>
+                    </div>
+                  );
+                default:
+                  return null;
+              }
+            })()}
+        </div>
+        <div className="w-full">
+          <AnimatedList>
+            {items.map((item, index) => (
+              <React.Fragment key={index}>
+                <ExpandableCard item={item} />
+              </React.Fragment>
+            ))}
+          </AnimatedList>
+        </div>
       </div>
-      <div className="w-full">
-        <AnimatedList>
-          {items.map((item, index) => (
-            <React.Fragment key={index}>
-              <ExpandableCard item={item} />
-            </React.Fragment>
-          ))}
-        </AnimatedList>
-      </div>
-    </div>
   );
 }
